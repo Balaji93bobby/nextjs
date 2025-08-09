22bd4c49affa9b253f86073fe2ffcdc6bfcce7ae
balaji93bobby <balaji93bobby@gmail.com>
2025-08-09 10:17:45 +0530

    created and practiced nested routes

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <>
+    <div>Dashboard Users</div>
+    <ul className='mt-18'>
+        <li>User1</li>
+        <li>User2</li>
+        <li>User3</li>
+        <li>User4</li>
+    </ul>
+    </>
+    
+  )
+}
+
+export default Users
