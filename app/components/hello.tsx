64ae24851f21cbf4c4359d22089d281f5ac80438
balaji93bobby <balaji93bobby@gmail.com>
2025-08-09 10:00:11 +0530

    create client side rendering componernt

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+
+function Hello(){
+    console.log('Hello Client')
+
+    return (
+        <div>
+            <h1> Hello form client</h1>
+        </div>
+    )
+}
+
+export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Hello from "./components/hello";
 
 export default function Home() {
+  console.log('what am I? --SERVER/CLIENT')
   return (
+    <>
     <h1 className="text-3xl">Welcome to Nextjs</h1>
+    <Hello />
+    </>
   );
 }
