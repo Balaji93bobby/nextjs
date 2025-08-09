6a82e1a95894c45ba9543308850be4a2775c589b
balaji93bobby <balaji93bobby@gmail.com>
2025-08-09 10:35:33 +0530

    used params to render page based on route for dynamic routing

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Users = () => {
   return (
     <>
     <div>Dashboard Users</div>
     <ul className='mt-18'>
-        <li>User1</li>
-        <li>User2</li>
-        <li>User3</li>
-        <li>User4</li>
+        <li>
+            <Link href='/dashboard/users/1'>
+            user 1
+            </Link>
+        </li>
+        <li>
+            <Link href='/dashboard/users/2'>
+            user 2
+            </Link>
+        </li>
+        <li>
+            <Link href='/dashboard/users/3'>
+            user 3
+            </Link>
+        </li>
+        <li>
+            <Link href='/dashboard/users/4'>
+            user 4
+            </Link>
+        </li>
     </ul>
     </>
     
